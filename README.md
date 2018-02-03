f5470d6ec32e7695139a00478b3d76cb541d21e4
David Tinoco <daviid.tc@gmail.com>
2018-02-03 19:05:26 +0100

    README.md modified and fixed a space in host.cfg and group/vars all

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # heat-ansible-wordpress
+
+To lauch this you need follow the next steps:
+  1. Clone this repository
+  2. Create and activate a virtualenv
+  3. Install with pip the following packages
+      - python-openstackclient
+      - python-heatclient
+      - ansible
+  4. Login with your script to openstack
+  5. Change your proyect parameters on "start.sh"
+  6. Load the private key that you need to login into your openstack instances
+  7. From repository directory execute "bash start.sh"
--- start.sh
+++ start.sh
@@ -13,8 +13,8 @@ function get_ip() {
     ip=`openstack server list | grep $1 | cut -d '|' -f 5 | cut -d ',' -f 2 | cut -d ' ' -f 2`
     sleep 2
   done
-  sed -i -e "s/ip$1/`openstack server list | grep $1 | cut -d '|' -f 5 | cut -d ',' -f 2`/g" ./hosts.cfg
-  sed -i -e "s/ip$1/`openstack server list | grep $1 | cut -d '|' -f 5 | cut -d ',' -f 2`/g" ./group_vars/all
+  sed -i -e "s/ip$1/`openstack server list | grep $1 | cut -d '|' -f 5 | cut -d ',' -f 2 | cut -d ' ' -f 2`/g" ./hosts.cfg
+  sed -i -e "s/ip$1/`openstack server list | grep $1 | cut -d '|' -f 5 | cut -d ',' -f 2 | cut -d ' ' -f 2`/g" ./group_vars/all
 }
 
 echo 'Get and stablish the ips on the template'
